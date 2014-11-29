9c1d2d93be1ea26649c6202f5b7689944bde7f28
Kovács Vince <vincekovacs@hotmail.com>
2014-11-29 02:11:16 +0100

    Create config table migration file

---
+++ src/migrations/2014_10_12_000000_create_config_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateConfigTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Schema::create('config', function (Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+            $table->string('key')
+                  ->unique();
+            $table->string('value');
+            $table->string('group');
+            $table->enum('type', ['int', 'bool', 'string']);
+            $table->unsignedInteger('modified_by_user_id');
+            $table->timestamp('modified_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        \Schema::drop('config');
+    }
+}
