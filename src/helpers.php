4c4d37ec40aa149f4cf74755aa683b387b0a2127
Kovács Vince <vincekovacs@hotmail.com>
2015-03-10 17:49:49 +0100

    config_db return DbConfig Loc instance if no parameter passed

--- src/helpers.php
+++ src/helpers.php
@@ -4,14 +4,18 @@ if (!function_exists('config_db')) {
     /**
      * Get config value by key
      *
-     * @param string $key config key
-     * @param mixed  $default
+     * @param string|null $key config key
+     * @param mixed       $default
      *
-     * @return mixed
+     * @return mixed|\ViKon\DbConfig\DbConfig
      *
      * @throws \ViKon\DbConfig\DbConfigException
      */
-    function config_db($key, $default = null) {
+    function config_db($key = null, $default = null) {
+        if ($key === null) {
+            return app('config.db');
+        }
+
         return app('config.db')->get($key, $default);
     }
 }
